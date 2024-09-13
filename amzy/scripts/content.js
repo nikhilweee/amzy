3c5bc8fece34b900a4016cef7b35d3197ae52f2d
Nikhil Verma <nikhilweee@gmail.com>
2024-09-13 05:07:58 -0400

    Add hide function

--- amzy/scripts/content.js
+++ amzy/scripts/content.js
@@ -1,15 +1,20 @@
-const resultsList = document.querySelector(
-  "[data-component-type='s-search-results']"
-);
-
-if (resultsList) {
-  // Proceed only if we are on a search results page
-  const resultItems = resultsList.querySelectorAll(
-    "[data-component-type='s-search-result']"
+async function processPage() {
+  const resultsList = document.querySelector(
+    "[data-component-type='s-search-results']"
   );
-  // Iterate over all products
-  for (const product of resultItems) {
-    addInsights(product);
+
+  if (resultsList) {
+    // Proceed only if we are on a search results page
+    const storage = await chrome.storage.local.get({ hidden: [] });
+    const resultItems = resultsList.querySelectorAll(
+      "[data-component-type='s-search-result']"
+    );
+
+    // Iterate over all products
+    for (const product of resultItems) {
+      addInsights(product);
+      addButton(product, storage);
+    }
   }
 }
 
@@ -33,20 +38,22 @@ async function addInsights(product) {
     // Add insights to the display area
     displayArea.innerHTML = insights.innerHTML;
     // Add event listeners to buttons
-    const buttons = displayArea.querySelectorAll("button");
-    for (const button of buttons) {
-      button.addEventListener("click", toggleButton);
+    const links = displayArea.querySelectorAll(
+      "[data-csa-c-action='infoPopOver']"
+    );
+    for (const link of links) {
+      link.addEventListener("click", insightsHandler);
     }
   }
 }
 
-function toggleButton(event) {
+function insightsHandler(event) {
   // event.target may be a span element inside the button
-  const button = event.target.closest("button");
+  const link = event.target.closest("a");
   // Parent element for all buttons and comments
-  const parent = button.closest("[data-hook='cr-insights-widget-aspects']");
+  const parent = link.closest("[data-hook='cr-insights-widget-aspects']");
   // ID of comments relevant to this button
-  const commentsID = button.id.replace("button", "bottom-sheet");
+  const commentsID = link.id.replace("button", "bottom-sheet");
   const comments = parent.querySelector(`#${commentsID}`);
   // Hide comments and exit if they are already visible
   if (comments.style.display === "block") {
@@ -60,3 +67,44 @@ function toggleButton(event) {
   }
   comments.style.display = "block";
 }
+
+function addButton(product, storage) {
+  const asin = product.dataset.asin;
+  // Add a button to hide or unhide the product
+  const container = product.querySelector("div.puis-atcb-add-container");
+  container.querySelector("span").style.display = "inline-block";
+  const span = document.createElement("span");
+  span.classList.add("a-button");
+  span.classList.add("a-button-base");
+  const button = document.createElement("button");
+  button.classList.add("a-button-text");
+  if (storage.hidden.includes(asin)) {
+    product.style.opacity = 0.2;
+    button.textContent = "Unhide";
+  } else {
+    button.textContent = "Hide";
+  }
+  button.addEventListener("click", buttonHandler);
+  span.appendChild(button);
+  container.appendChild(span);
+}
+
+function buttonHandler(e) {
+  const product = e.target.closest("[data-asin]");
+  const asin = product.dataset.asin;
+  chrome.storage.local.get({ hidden: [] }).then((storage) => {
+    let hidden = storage.hidden;
+    if (hidden.includes(asin)) {
+      product.style.opacity = 1;
+      hidden = hidden.filter((item) => item !== asin);
+      e.target.textContent = "Hide";
+    } else {
+      product.style.opacity = 0.2;
+      hidden.push(asin);
+      e.target.textContent = "Unhide";
+    }
+    chrome.storage.local.set({ hidden });
+  });
+}
+
+document.addEventListener("DOMContentLoaded", processPage());
